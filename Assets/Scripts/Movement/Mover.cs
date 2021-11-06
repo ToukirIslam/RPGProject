11ec37cf17dffce36b483fc7ec5d04c84b936e11
Toukir islam <gameshovo@gmail.com>
2021-11-07 00:46:06 +0100

    adding animation , move player with code

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator character;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hashit = Physics.Raycast(ray, out hit);
+        if (hashit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity); //use it to make it local velocity 
+        float speed = localVelocity.z;
+        character.SetFloat("ForwardSpeed", speed);
+    }
+    
+}
